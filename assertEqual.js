5815220ea2c38dd97d50cf59fe10f30be901fe8c
USBrido <obrido@gmail.com>
2019-11-25 21:23:00 +0000

    assetEqual first commit

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('👯‍👯‍👯‍Assertion Passed: ' + [actual] + ' === ' + [expected] + '.');
+  } else {
+    console.log('🙅🏻‍♂️🙅🏻‍♂️🙅🏻‍Assertion Failed:' + [actual] + ' !== ' + [expected] + '.');
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
