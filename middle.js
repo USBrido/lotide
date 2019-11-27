525007eb1f7812b83b329503585d9a223a57843c
USBrido <obrido@gmail.com>
2019-11-27 19:19:52 +0000

    first commit for middle.js incomplete

---
+++ middle.js
@@ -0,0 +1,32 @@
+function eqArrays(arr1, arr2) {
+  for (i = 0; i < arr1.length; i++) {
+      if ( arr1[i] !== arr2[i]) 
+      return false
+      
+    }
+  return true
+  
+}
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`👯‍👯‍👯‍Assertion Passed: ${arr1} === ${arr2}.`);
+  } else {
+    console.log(`🙅🏻‍♂️🙅🏻‍♂️🙅🏻‍Assertion Failed: ${arr1} !== ${arr2}.`);
+  }
+};
+ 
+function middle(arr) {
+let midArray = arr.indexOf[i];
+if (arr.length === 1 || arr.length === 2) {
+return [];
+} else if (arr.length % 2) {
+return arr
+} else if (arr.length % 3) {
+
+}
+}
+
+
+
+
+
