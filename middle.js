d4926afef02b51bdbc6d86669dfb45aad62de439
USBrido <obrido@gmail.com>
2019-11-27 22:59:10 +0000

    another commit for middle.js
    .exit

--- middle.js
+++ middle.js
@@ -1,32 +1,35 @@
-function eqArrays(arr1, arr2) {
-  for (i = 0; i < arr1.length; i++) {
-      if ( arr1[i] !== arr2[i]) 
-      return false
+// function eqArrays(arr1, arr2) {
+//   for (i = 0; i < arr1.length; i++) {
+//       if ( arr1[i] !== arr2[i]) 
+//       return false
       
-    }
-  return true
+//     }
+//   return true
   
-}
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`👯‍👯‍👯‍Assertion Passed: ${arr1} === ${arr2}.`);
-  } else {
-    console.log(`🙅🏻‍♂️🙅🏻‍♂️🙅🏻‍Assertion Failed: ${arr1} !== ${arr2}.`);
-  }
-};
+// }
+// const assertArraysEqual = function(arr1, arr2) {
+//   if (eqArrays(arr1, arr2)) {
+//     console.log(`👯‍👯‍👯‍Assertion Passed: ${arr1} === ${arr2}.`);
+//   } else {
+//     console.log(`🙅🏻‍♂️🙅🏻‍♂️🙅🏻‍Assertion Failed: ${arr1} !== ${arr2}.`);
+//   }
+// };
  
-function middle(arr) {
-let midArray = arr.indexOf[i];
-if (arr.length === 1 || arr.length === 2) {
-return [];
-} else if (arr.length % 2) {
-return arr
-} else if (arr.length % 3) {
-
+function middle(array) {
+  let count = array.length;
+  let i = ;
+  if (count === 1 || count === 2) {
+    return [];
+  } else if (count % 2 === 0) {
+    return array.indexOf(count);
+  } else if (array.length % 2 !== 0) {
+    return (array.indexOf(count), array.indexOf(count + 1));
 }
 }
-
-
+console.log(middle([1, 2, 3, 4]));// [2, 3]);
+console.log(middle([1, 2, 3, 4, 5, 6]));// [3, 4]);
+console.log(middle([1, 2, 3])); // => [2]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
 
 
 
