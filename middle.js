2d926c2d744303d215fee44ee1e0dd53e51a0ebb
USBrido <obrido@gmail.com>
2019-11-29 05:03:13 +0000

    middle.js commit, I feel theres a way of doing this  that's easier

--- middle.js
+++ middle.js
@@ -1,35 +1,39 @@
-// function eqArrays(arr1, arr2) {
-//   for (i = 0; i < arr1.length; i++) {
-//       if ( arr1[i] !== arr2[i]) 
-//       return false
+function eqArrays(arr1, arr2) {
+  for (i = 0; i < arr1.length; i++) {
+      if ( arr1[i] !== arr2[i]) 
+      return false
       
-//     }
-//   return true
+    }
+  return true
   
-// }
-// const assertArraysEqual = function(arr1, arr2) {
-//   if (eqArrays(arr1, arr2)) {
-//     console.log(`👯‍👯‍👯‍Assertion Passed: ${arr1} === ${arr2}.`);
-//   } else {
-//     console.log(`🙅🏻‍♂️🙅🏻‍♂️🙅🏻‍Assertion Failed: ${arr1} !== ${arr2}.`);
-//   }
-// };
+}
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`👯‍👯‍👯‍Assertion Passed: ${arr1} === ${arr2}.`);
+  } else {
+    console.log(`🙅🏻‍♂️🙅🏻‍♂️🙅🏻‍Assertion Failed: ${arr1} !== ${arr2}.`);
+  }
+};
  
 function middle(array) {
-  let count = array.length;
-  let i = ;
-  if (count === 1 || count === 2) {
+  let arrLength = array.length;
+  let midArray = Math.floor(arrLength/2);
+  let count = [] ;
+  if (arrLength === 1 || arrLength === 2) {
     return [];
-  } else if (count % 2 === 0) {
-    return array.indexOf(count);
+  } else if (arrLength % 2 === 0) {
+    count.push(array[midArray - 1])
+    count.push(array[midArray])
+    return count ;
   } else if (array.length % 2 !== 0) {
-    return (array.indexOf(count), array.indexOf(count + 1));
+    count.push(array[midArray])
+    return count;
 }
 }
-console.log(middle([1, 2, 3, 4]));// [2, 3]);
-console.log(middle([1, 2, 3, 4, 5, 6]));// [3, 4]);
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
+assertArraysEqual((middle([1, 2, 3, 4])), [2, 3]);
+assertArraysEqual((middle([1, 2, 3, 4, 5, 6])), [3, 4]);
+assertArraysEqual((middle([1, 2, 3])), [2]);
+assertArraysEqual((middle([1, 2, 3, 4, 5])), [3]);
 
 
 
